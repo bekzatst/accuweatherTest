3daf41b17b943aa74d16e732c4194f7ad1bfb9cc
bekzatst <bekzzaatt@gmail.com>
2024-11-26 17:00:30 +0500

    Added Allure Report elements

--- src/test/java/tests/AccuweatherTest.java
+++ src/test/java/tests/AccuweatherTest.java
@@ -1,21 +1,45 @@
 package tests;
 
+import io.qameta.allure.*;
+import io.qameta.allure.testng.Tag;
+import org.openqa.selenium.NoSuchElementException;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import pages.CityWeatherPage;
 import pages.MainPage;
 
+import java.io.ByteArrayInputStream;
+
+import static io.qameta.allure.Allure.addAttachment;
+import static io.qameta.allure.Allure.step;
+
 public class AccuweatherTest extends BaseTest {
     MainPage mainPage = new MainPage();
     CityWeatherPage cityWeatherPage = new CityWeatherPage();
 
-    @Test
+    @Test(description = "Verify the search functionality and open the city weather page")
+    @Severity(SeverityLevel.CRITICAL)
+    @Epic("User Management")
+    @Feature("Search")
+    @Story("Search for the City and Open the City Weather Page")
+    @Owner("Beks")
+    @Tag("Regression")
     public void testCitySearch() {
+        step("Consent Data Usage");
         mainPage.consentDataUsage();
-        mainPage.inputCityName();
-        Assert.assertTrue(mainPage.isSearchResultsListDisplayed());
+        try {
+            step("Input 'New York' in the search field", () -> {
+                mainPage.inputCityName();
+                Assert.assertTrue(mainPage.isSearchResultsListDisplayed(), "Search Results List is Not Displayed");
+            });
+        } catch (NoSuchElementException | AssertionError e) {
+            byte[] screenshot = browser.getScreenshot();
+            addAttachment("Screenshot of search results", new ByteArrayInputStream(screenshot));
+            throw e;
+        }
+        step("Click on the first search result");
         mainPage.clickFirstSearchResult();
-        Assert.assertTrue(cityWeatherPage.state().waitForDisplayed());
-        Assert.assertTrue(cityWeatherPage.headerContainsCityName());
+        Assert.assertTrue(cityWeatherPage.state().waitForDisplayed(), "City Weather Page is Not Displayed");
+        Assert.assertTrue(cityWeatherPage.headerContainsCityName(), "City Weather Page Header Does Not contain the City Name from the Search");
     }
 }
--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -6,19 +6,25 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import utils.SettingsTestData;
 
+import static io.qameta.allure.Allure.step;
+
 public class BaseTest {
     protected Browser browser;
 
     @BeforeMethod
     public void setup() {
+        step("Open the browser");
         browser = AqualityServices.getBrowser();
+        step("Maximize the browser window");
         browser.maximize();
+        step("Go to start URL");
         browser.goTo(SettingsTestData.getEnvData().getHost());
     }
 
     @AfterMethod
     public void teardown() {
         if (AqualityServices.isBrowserStarted()) {
+            step("Quitting the browser");
             browser.quit();
         }
     }
